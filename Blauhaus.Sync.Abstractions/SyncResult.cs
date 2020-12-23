336553cdf8a23a59a62b9a5840fde35c4fbbbab9
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-12-23 09:02:17 +0200

    no message

--- Blauhaus.Sync.Abstractions/SyncResult.cs
+++ Blauhaus.Sync.Abstractions/SyncResult.cs
@@ -4,7 +4,6 @@ using Blauhaus.Domain.Abstractions.Entities;
 namespace Blauhaus.Sync.Abstractions
 {
     public class SyncResult<TPayload>  
-        where TPayload : IEntity
     {
         public List<TPayload> EntityBatch { get; set; } = new List<TPayload>();
         public long TotalActiveEntityCount { get; set; }

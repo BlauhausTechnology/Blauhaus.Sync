f215b8c2850473e61ba4fa7dc746577c250629e6
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-12-23 09:01:13 +0200

    no message

---
+++ Blauhaus.Sync.Abstractions/SyncCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blauhaus.Sync.Abstractions
+{
+    public class SyncCommand
+    {
+        /// <summary>
+        /// Used only when asking for entities that have been modified after the last sync. Returns older entities first
+        /// </summary>
+        public long? NewerThan { get; set; }
+
+        /// <summary>
+        /// Used when syncing additional entities after an initial set has been downloaded. Returns newer entities first. 
+        /// </summary>
+        public long? OlderThan { get; set; }
+        
+        /// <summary>
+        /// Specifies the number of entities to be downloaded from the server and published to the client in each batch
+        /// </summary>
+        public int BatchSize { get; set; } = 100;
+
+        /// <summary>
+        /// If provided then only the entity matching this Id will be returned, and only NewerThan will be considered
+        /// </summary>
+        public Guid? Id { get; set; } = null;
+
+        /// <summary>
+        /// Addidtional properties if needed to filter the sync. Must be the same across multiple syncs with the same intent!
+        /// </summary>
+        public Dictionary<string, string>? Filters { get; set; } = null;
+    }
+}
---
+++ Blauhaus.Sync.Abstractions/SyncResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Blauhaus.Domain.Abstractions.Entities;
+
+namespace Blauhaus.Sync.Abstractions
+{
+    public class SyncResult<TPayload>  
+        where TPayload : IEntity
+    {
+        public List<TPayload> EntityBatch { get; set; } = new List<TPayload>();
+        public long TotalActiveEntityCount { get; set; }
+        public long EntitiesToDownloadCount { get; set; }
+    }
+}

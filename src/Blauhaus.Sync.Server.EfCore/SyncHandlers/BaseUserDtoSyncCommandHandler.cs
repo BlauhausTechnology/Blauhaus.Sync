2f46a190e27c6f01e5634986af51407a82cd6f1a
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-10-11 12:12:35 +0200

    no message

---
+++ src/Blauhaus.Sync.Server.EfCore/SyncHandlers/BaseUserDtoSyncCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Blauhaus.Domain.Abstractions.CommandHandlers;
+using Blauhaus.Domain.Abstractions.DtoHandlers;
+using Blauhaus.Domain.Abstractions.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Blauhaus.Analytics.Abstractions.Service;
+using Blauhaus.Common.Abstractions;
+using Blauhaus.Time.Abstractions;
+
+namespace Blauhaus.Sync.Server.EfCore.SyncHandlers
+{
+    public abstract class BaseUserDtoSyncCommandHandler<TDbContext, TEntity, TDto, TId, TUser> : BaseDtoSyncCommandHandler<TDbContext, TEntity, TDto, TId, TUser>
+        where TDto : IClientEntity<TId>, IHasUserId
+        where TId : IEquatable<TId>
+        where TEntity: class, IServerEntity, IDtoOwner<TDto>, IHasUserId
+        where TDbContext : DbContext
+    {
+        protected BaseUserDtoSyncCommandHandler(
+            IAnalyticsService analyticsService, 
+            ITimeService timeService, 
+            Func<TDbContext> dbContextFactory) 
+                : base(analyticsService, timeService, dbContextFactory)
+        {
+        }
+    }
+}

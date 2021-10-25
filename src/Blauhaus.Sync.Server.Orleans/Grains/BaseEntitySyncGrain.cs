6cb0a962af4b3b26318f1995e164697a327ae5a2
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-10-25 18:11:18 +0200

    no message

---
+++ src/Blauhaus.Sync.Server.Orleans/Grains/BaseEntitySyncGrain.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading.Tasks;
+using Blauhaus.Analytics.Abstractions.Service;
+using Blauhaus.Auth.Abstractions.User;
+using Blauhaus.Domain.Abstractions.CommandHandlers;
+using Blauhaus.Domain.Abstractions.Commands;
+using Blauhaus.Domain.Abstractions.DtoHandlers;
+using Blauhaus.Domain.Abstractions.Entities;
+using Blauhaus.Domain.Server.Entities;
+using Blauhaus.Orleans.Abstractions.Resolver;
+using Blauhaus.Orleans.EfCore.Grains;
+using Blauhaus.Responses;
+using Blauhaus.Sync.Server.Orleans.Abstractions;
+using Blauhaus.Time.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blauhaus.Sync.Server.Orleans.Grains
+{
+    public abstract class BaseEntitySyncGrain<TDbContext, TEntity, TDto, TDtoSyncGrain, TGrainResolver> : BaseEntityGrain<TDbContext, TEntity, TDto, TGrainResolver>,
+        IVoidAuthenticatedCommandHandler<ActivateCommand, IAuthenticatedUser>,
+        IVoidAuthenticatedCommandHandler<ArchiveCommand, IAuthenticatedUser>
+        where TDbContext : DbContext
+        where TEntity : BaseServerEntity, IDtoOwner<TDto>
+        where TDto : IClientEntity<Guid>
+        where TGrainResolver : IGrainResolver
+        where TDtoSyncGrain : IDtoSyncGrain<TDto>
+    {
+
+        protected TDtoSyncGrain DtoSyncGrain = default!;
+
+        protected BaseEntitySyncGrain(
+            Func<TDbContext> dbContextFactory,
+            IAnalyticsService analyticsService,
+            ITimeService timeService,
+            TGrainResolver grainResolver)
+            : base(dbContextFactory, analyticsService, timeService, grainResolver)
+        {
+        }
+
+
+        public override Task OnActivateAsync()
+        {
+            DtoSyncGrain = GrainResolver.ResolveSingleton<TDtoSyncGrain>();
+            return base.OnActivateAsync();
+        }
+         
+        protected override async Task<Response> HandleActivatedAsync(TEntity loadedEntity)
+        {
+            var dto = await LoadedEntity.GetDtoAsync();
+            await DtoSyncGrain.UpdateDtoAsync(dto);
+            return Response.Success();
+        }
+
+        protected override async Task<Response> HandleArchivedAsync(TEntity entity)
+        {
+            var dto = await LoadedEntity.GetDtoAsync();
+            await DtoSyncGrain.UpdateDtoAsync(dto);
+            return Response.Success();
+        }
+
+        protected override async Task<Response> HandleDeletedAsync(TEntity entity)
+        {
+            var dto = await LoadedEntity.GetDtoAsync();
+            await DtoSyncGrain.UpdateDtoAsync(dto);
+            return Response.Success();
+        }
+    }
+
+}
---
+++ src/Blauhaus.Sync.TestHelpers.Orleans/BaseEntitySyncGrainTests/BaseEntitySyncGrainActivateTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using Blauhaus.Auth.Abstractions.User;
+using Blauhaus.Domain.Abstractions.CommandHandlers;
+using Blauhaus.Domain.Abstractions.Commands;
+using Blauhaus.Domain.Abstractions.DtoHandlers;
+using Blauhaus.Domain.Abstractions.Entities;
+using Blauhaus.Domain.Server.Entities;
+using Blauhaus.Domain.TestHelpers.EntityBuilders;
+using Blauhaus.Orleans.Abstractions.Resolver;
+using Blauhaus.Orleans.TestHelpers.BaseEntityGrainTests;
+using Blauhaus.Sync.Server.Orleans.Abstractions;
+using Blauhaus.Sync.Server.Orleans.Grains;
+using Blauhaus.TestHelpers.MockBuilders;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using EntityState = Blauhaus.Domain.Abstractions.Entities.EntityState;
+
+namespace Blauhaus.Sync.TestHelpers.Orleans.BaseEntitySyncGrainTests
+{
+    public abstract class BaseEntitySyncGrainActivateTests<TDbContext, TGrain, TEntity, TEntityBuilder, TGrainResolver, TDto, TSyncGrain> 
+        : BaseEntityGrainActivateTests<TDbContext, TGrain, TEntity, TEntityBuilder, TGrainResolver, TDto>
+        where TGrain: BaseEntitySyncGrain<TDbContext, TEntity, TDto, TSyncGrain, TGrainResolver>, IVoidAuthenticatedCommandHandler<ActivateCommand, IAuthenticatedUser> 
+        where TEntity : BaseServerEntity, IDtoOwner<TDto>
+        where TEntityBuilder : BaseServerEntityBuilder<TEntityBuilder, TEntity>
+        where TDbContext : DbContext
+        where TSyncGrain : class, IDtoSyncGrain<TDto>
+        where TDto : IClientEntity<Guid>
+        where TGrainResolver : class, IGrainResolver
+    {
+
+        protected MockBuilder<TSyncGrain> MockDtoSyncGrain = null!;
+
+        public override void Setup()
+        {
+            base.Setup();
+
+            MockDtoSyncGrain = new MockBuilder<TSyncGrain>();
+            
+            var mockGrainResolver = new MockBuilder<TGrainResolver>();
+            mockGrainResolver.Mock.Setup(x => x.ResolveSingleton<TSyncGrain>()).Returns(MockDtoSyncGrain.Object);
+            AddSiloService(mockGrainResolver.Object);
+        } 
+         
+                
+        [TestCase(EntityState.Draft)]
+        [TestCase(EntityState.Archived)]
+        public async Task WHEN_activated_SHOULD_publish_Dto(EntityState validState)
+        {
+            //Arrange
+            ExistingEntityBuilder.With(x => x.EntityState, validState);
+
+            //Act
+            await Sut.HandleAsync(Command, AdminUser);
+
+            //Assert 
+            MockDtoSyncGrain.Verify(x => x.UpdateDtoAsync(It.Is<TDto>(y =>
+                y.Id == GrainId &&
+                y.EntityState == EntityState.Active)));
+        }
+    }
+}

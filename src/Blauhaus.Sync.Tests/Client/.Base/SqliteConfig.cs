470f970d09fa7da26a46be740309965f0413d24d
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-12-23 08:11:53 +0200

    no message

--- src/Blauhaus.Sync.Tests/Client/.Base/SqliteConfig.cs
+++ src/Blauhaus.Sync.Tests/Client/.Base/SqliteConfig.cs
@@ -1,17 +1,8 @@
-﻿using Blauhaus.Analytics.Abstractions.Service;
-using Blauhaus.Analytics.TestHelpers.MockBuilders;
-using Blauhaus.ClientDatabase.Sqlite.Config;
-using Blauhaus.ClientDatabase.Sqlite.Service;
+﻿using Blauhaus.ClientDatabase.Sqlite.Config;
 using Blauhaus.DeviceServices.Abstractions.DeviceInfo;
-using Blauhaus.DeviceServices.TestHelpers.MockBuilders; 
-using NUnit.Framework;
-using SQLite;
 using System.Collections.Generic;
 using System;
-using System.Threading.Tasks;
-using Blauhaus.Sync.TestHelpers.Sqlite;
 using Blauhaus.Sync.Tests.Client.TestObjects;
-using Blauhaus.Sync.Tests.TestObjects;
 using Blauhaus.Sync.Tests.TestObjects.User;
 
 namespace Blauhaus.Sync.Tests.Client.Base
